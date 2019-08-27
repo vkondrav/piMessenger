103b45209a97106ce7d3aab4ba8722782d30959e
vkondrav <kondrav@live.ca>
2019-08-27 17:22:48 -0400

    updating reload function

--- app.sh
+++ app.sh
@@ -5,6 +5,8 @@ command=$1
 if [ $command == "reload" ]; then
 	sudo systemctl stop pimessenger.service
 	sudo systemctl start pimessenger.service
+        sudo service motion stop
+        sudo service motion start
 else
 	sudo systemctl $command pimessenger.service
 fi
