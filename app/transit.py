a78822d2d3620da6ace0af4e6282cfe0d40c687e
vkondrav <kondrav@live.ca>
2020-02-15 09:56:55 -0500

    updating transit

--- app/transit.py
+++ app/transit.py
@@ -21,27 +21,30 @@ class TransitThread(threading.Thread):
 		self.soundFile = soundFile
 		threading.Thread.__init__(self)
 
+	def clock(self):
+		return datetime.now(self.timezone).strftime("%I:%M%p")
+
 	def predict(self, url):
 		print (url)
 
 		r = requests.get(url)
 
 		if(r.status_code != 200):
-			return
+			return None
 			
 		json = r.json()
 		if "predictions" not in json:
-			return
+			return None
 
 		p = json["predictions"]
 		title = p["routeTitle"]
 		if "direction" not in p:
-			return
+			return None
 
 		direction = p["direction"]
 
 		if "prediction" not in direction:
-			return
+			return None
 		
 		prediction = direction["prediction"]
 
@@ -53,17 +56,16 @@ class TransitThread(threading.Thread):
 			current = prediction
 
 		if "minutes" not in current:
-			return
+			return None
 		
 		minutes = current["minutes"]
 
-		message = title + " arriving in " + minutes + " minutes"
-
 		if(int(minutes) > self.limit):
-			return
+			return None
 
-		SoundThread(self.soundFile).start()
-		SpeechThread(message).start()
+		message = self.clock() + " " + title + " arriving in " + minutes + " minutes"
+
+		return message
 
 	def inTimeSlot(self):
 		now = datetime.now(self.timezone)
@@ -80,6 +82,11 @@ class TransitThread(threading.Thread):
 		while True:
 
 			if(self.inTimeSlot()):
-				self.predict(url)
+				message = self.predict(url)
+				if(message is None):
+					message = self.clock()
+				print(message)
+				SoundThread(self.soundFile).start()
+				SpeechThread(message).start()
 
 			time.sleep(self.pollRate)
